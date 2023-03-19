16550b151616a4148a219c4493af596b44086561
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2023-03-19 23:49:55 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -12,25 +12,27 @@ function handleInput(evt) {
   } = evt.target;
   console.log(delay.value, step.value, amount.value);
 
-  delayPr = Number(form.elements.delay.value);
-  stepPr = Number(form.elements.step.value);
-  amountPr = Number(form.elements.amount.value);
+  let delayPr = Number(form.elements.delay.value);
+  let stepPr = Number(form.elements.step.value);
+  let amountPr = Number(form.elements.amount.value);
 
   for (let position = 1; position <= amountPr; position++) {
     console.log(delayPr);
-    delayPr += stepPr;
+  
     createPromise(position, delayPr)
       .then(({ position, delayPr }) =>
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${delayPr}ms`
-        )
+        ) 
       )
       .catch(({ position, delayPr }) =>
         Notiflix.Notify.failure(
           `❌ Rejected promise ${position} in ${delayPr}ms`
         )
-      );
+    );
+     delayPr += stepPr;
   }
+  // evt.target.reset();
 }
 
 function createPromise(position, delay) {
